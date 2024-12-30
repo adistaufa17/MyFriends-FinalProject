179e7f8c0cca1851d3d763a8fee139563a1ef6df
adistaufa17 <adistanurulfadilah01@gmail.com>
2024-12-31 06:41:04 +0700

    placeholder dan loading / progressbar

--- app/src/main/java/com/adista/finalproject/util/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/adista/finalproject/util/ImageViewBindingAdapter.kt
@@ -2,7 +2,10 @@ package com.adista.finalproject.util
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.adista.finalproject.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 
 object ImageViewBindingAdapter {
 
@@ -13,6 +16,12 @@ object ImageViewBindingAdapter {
         urlImage?.let { value ->
             Glide.with(context)
                 .load(value)
+                .apply(
+                    RequestOptions()
+                    .placeholder(R.drawable.loading_animation) // Add a loading animation drawable
+                    .error(R.drawable.image_placeholder) // Add an error placeholder drawable
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .centerCrop())
                 .into(this)
         }
     }
